d96a977b854e8fc5b93a0f5a7ee2fab0fd852646
xzerad <radwan.chaieb@gmail.com>
2022-12-01 21:54:37 +0100

    create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# top_g_easter_eggs
+
+A project with flutter for "nuit de l'info" hackathon the subject is to make an application with hidden easter eggs
---
+++ android/app/src/main/kotlin/com/example/top_g_easter_eggs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.top_g_easter_eggs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
